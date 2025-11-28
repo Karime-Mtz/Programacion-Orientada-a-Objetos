bd90d775d663b3eb63205e87a747971d1da74454
Karime-Mtz <A01709690@tec.mx>
2025-11-27 22:21:22 -0600

    Update Libro.h

--- Libro.h
+++ Libro.h
@@ -1,57 +1,100 @@
+#ifndef LIBRO_H
+#define LIBRO_H
+
 #include <string>
 #include <iostream>
+#include "Valoracion.h"
 using namespace std;
 
 
 class Libro {
     private:
         string titulo;
-        string genero;
         string autor;
         int num_pags;
+        Valoracion reviews[20];
+        int num_reviews;
 
     public:
         Libro();
-        Libro(string title, string gender, string aut, int pags);
+        Libro(string title, string aut, int pags);
 
         void mostrar_info_libro();
 
+        float getPromedio_calificacion();
+        string getTitulo();
+        string getAutor();
+        int getNum_pags();
+
         void setTitulo(string title);
-        void setGenero(string gender);
         void setAutor(string author);
+        void setNum_pags(int pags);
+
+        void agregar_review(Valoracion r);
+        
 
 };
 
 Libro::Libro(){
     titulo = "Desconocido";
-    genero = "Desconocido";
     autor = "Desconocido";
     num_pags = 0;
+    num_reviews = 0;
 }
 
-Libro::Libro(string title, string gender, string aut, int pags){
+Libro::Libro(string title, string aut, int pags){
     titulo = title;
-    genero = gender;
     autor = aut;
     num_pags = pags;
+    num_reviews = 0;
 }
 
+
 void Libro::mostrar_info_libro(){
-    cout << titulo << " - ";
-    cout << genero << " - ";
-    cout << autor << " - ";
-    cout << num_pags << endl;
+    cout << "Título: " << titulo << endl;
+    cout << "Autor: " <<autor << endl;
+    cout << "Número de páginas: " << num_pags << endl;
+}
+
+string Libro::getTitulo(){
+    return titulo;
+}
+
+string Libro::getAutor(){
+    return autor;
+}
+
+int Libro::getNum_pags(){
+    return num_pags;
 }
 
 void Libro::setTitulo(string title){
     titulo = title;
 }
 
-void Libro::setGenero(string gender){
-    genero = gender;
-}
 
 void Libro::setAutor(string author){
     autor = author;
 }
 
+void Libro::setNum_pags(int pags){
+    num_pags = pags;
+}
+
+float Libro::getPromedio_calificacion(){
+    float suma = 0.0;
+    for (int i = 0; i < num_reviews; i++){
+        suma = suma + reviews[i].getCalificacion();
+    }
+    suma = suma / num_reviews;
+    return suma;
+}
+
+void Libro::agregar_review(Valoracion r){
+    if (num_reviews < 20) {
+        reviews[num_reviews] = r;
+        num_reviews += 1;
+    }
+}
+
+#endif
