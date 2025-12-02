3bb2d2f4ce88348aaba92b4fae78449edfd437ee
Karime-Mtz <A01709690@tec.mx>
2025-12-02 11:24:42 -0600

    Update Comic.h

--- Comic.h
+++ Comic.h
@@ -6,41 +6,27 @@ using namespace std;
 class Comic: public Libro{
     private:
         string universo; 
-        string ilustrador;
     public:
             Comic();
-            Comic(string t, string a, int p, string u, string i);
+            Comic(string t, string a, bool d, string u);
             
             string getUniverso();
-            string getIlustrador();
-
             void setUniverso(string u);
-            void setIlustrador(string drawer);
-
 };
 
 Comic::Comic() : Libro(){
     universo = "Desconocido";
-    ilustrador = "Desconocido;";
 }
 
-Comic::Comic(string t, string a, int p, string u, string i) : Libro(t,a,p){
+Comic::Comic(string t, string a, bool d, string u) : Libro(t,a,d){
     universo = u;
-    ilustrador = i;
 }
 
 string Comic::getUniverso(){
     return universo;
 }
 
-string Comic::getIlustrador(){
-    return ilustrador;
-}
-
 void Comic::setUniverso(string u){
     universo = u;
 }
 
-void Comic::setIlustrador(string drawer){
-    ilustrador = drawer;
-}
