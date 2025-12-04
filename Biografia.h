888d70f3187eb9ccd9dbd3c21867278cdfb6c801
Karime-Mtz <A01709690@tec.mx>
2025-12-04 12:54:30 -0600

    Update Biografia.h

--- Biografia.h
+++ Biografia.h
@@ -1,33 +1,77 @@
+/*
+ * Proyecto Biblioteca Digital
+ * Karime Martínez López
+ * A01709690
+ * 03/12/2025
+ */
+
+ /*
+ * Clase Biografia que hereda de clase Libro. Contiene los métodos
+ * genéricos para manejar sus atributos especiales.
+ *
+ */
+
 #include <string>
 #include <iostream>
 #include "Libro.h"
 using namespace std;
 
+// Declaracion de la clase Biografia que hereda de clase Libro
 class Biografia: public Libro {
-    private:
-        string personaje;
-    public:
-            Biografia();
-            Biografia(string t, string a, int p, string e);
 
-            string getPersonaje();
-            void setPersonaje(string persona);
+    private:
+        // Declaracion de variables de instancia
+        string personaje;
+
+    public:
+        // Declaracion de los metodos del objeto
+        Biografia();
+        Biografia(string t, string a, bool d, string e);
+
+        string get_personaje();
+        void set_personaje(string persona);
 
 
 };
 
+/**
+ * Constructor por default
+ *
+ * @param
+ * @return Objeto Biografia
+ */
 Biografia::Biografia(): Libro(){
     personaje = "Desconocido";
 }
 
-Biografia:: Biografia(string t, string a, int p, string e) : Libro(t,a,p){
+/**
+ * Constructor que recibe valores para llenar las variables de instancia
+ * de este objeto, además de las variables heredadas
+ *
+ * @param string string t: titulo del libro, string a: autor, bool d: disponibilidad
+ * string e: personaje del que trata la biografia
+ * @return objeto Biografia
+ */
+Biografia:: Biografia(string t, string a, bool d, string e) : Libro(t,a,d){
     personaje = e;
 }
 
-string Biografia::getPersonaje(){
+/**
+ * getter personaje
+ *
+ * @param
+ * @return string: personaje principal
+*/
+string Biografia::get_personaje(){
     return personaje;
 }
 
-void Biografia::setPersonaje(string persona){
+/**
+ * setter personaje
+ *
+ * @param string persona: personaje de la biografia
+ * @return 
+*/
+void Biografia::set_personaje(string persona){
     personaje = persona;
 }
