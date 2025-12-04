d6141ce785c812ca98484e4e453f456f4738ace8
Karime-Mtz <A01709690@tec.mx>
2025-12-04 12:53:43 -0600

    Update Valoracion.h

--- Valoracion.h
+++ Valoracion.h
@@ -1,3 +1,16 @@
+/*
+ * Proyecto Biblioteca Digital
+ * Karime Martínez López
+ * A01709690
+ * 03/12/2025
+ */
+
+ /*
+ * Clase Valoracion contiene los métodos genéricos
+ * para el manejo de sus atributos.
+ *
+ */
+
 #ifndef VALORACION_H
 #define VALORACION_H
 
@@ -5,63 +18,66 @@
 #include <iostream>
 using namespace std;
 
+// Declaracion de clase Valoracion
 class Valoracion {
+
     private:
+        // Declaracion de variables de instancia
         string comentario;
         int calificacion;
     
     public:
+        // Declaracion de los metodos del objeto
         Valoracion();
         Valoracion(string comment, int cali);
 
-        string mostrar_valoracion();
+        int get_calificacion();
+        void set_calificacion(int n);
 
-        string getComentario();
-        int getCalificacion();
-
-        void setCalificacion(int n);
-
-        string agregar_comentario(string &comment);
+        string agregar_comentario(string comment);
 };
 
+/**
+ * Constructor por default
+ *
+ * @param
+ * @return Objeto Valoracion
+ */
 Valoracion::Valoracion(){
     comentario = "";
     calificacion = 0;
 }
 
-
+/**
+ * Constructor que recibe valores para llenar las variables de instancia
+ *
+ * @param string comment: comentarios acerca del libro leido, 
+ * int cali: calificacion que se le da un libro
+ * @return objeto Valoracion
+ */
 Valoracion::Valoracion(string comment, int cali){
     comentario = comment;
     calificacion = cali;
 }
 
-string Valoracion::mostrar_valoracion(){
-    stringstream info_review;
-
-    if (comentario.length() == 0){
-        info_review << "No hay comentarios" << "\n";
-    } else {
-        info_review << "Comentarios: " << comentario << "\n";
-    }
-    info_review << "Puntuacion: " << calificacion << "\n";
-    
-    return info_review.str();
-}
-
-
-int Valoracion::getCalificacion(){
+/**
+ * getter calificacion
+ *
+ * @param
+ * @return int: numero que representa la calificacion 
+*/
+int Valoracion::get_calificacion(){
     return calificacion;
 }
 
-void Valoracion::setCalificacion(int c){
+/**
+ * setter calificacion
+ *
+ * @param int c: calificacion asignada
+ * @return 
+*/
+void Valoracion::set_calificacion(int c){
     calificacion = c;
-}
-
-
-//Checar si si se ocupa &
-string Valoracion::agregar_comentario(string &comment){
-    comentario = comentario + "\n" + comment;
-    return comentario;
 }
 
 
