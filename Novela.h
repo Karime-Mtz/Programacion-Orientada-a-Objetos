dfa8fe5547f0600b2720a5f98886c88e27c33d03
Karime-Mtz <A01709690@tec.mx>
2025-12-04 12:54:01 -0600

    Update Novela.h

--- Novela.h
+++ Novela.h
@@ -1,48 +1,103 @@
+/*
+ * Proyecto Biblioteca Digital
+ * Karime Martínez López
+ * A01709690
+ * 03/12/2025
+ */
+
+ /*
+ * Clase Novela que hereda de clase Libro. Contiene los métodos
+ * genéricos para manejar sus atributos especiales.
+ *
+ */
+
 #include <string>
 #include <iostream>
 #include "Libro.h"
 using namespace std;
 
+// Declaracion de clase Novela que hereda de clase Libro
 class Novela : public Libro{
+
     private:
+        // Declaracion de variables de instancia
         string genero; 
         bool es_saga;
+
     public:
+            // Declaracion de los metodos del objeto
             Novela();
             Novela(string t, string a, bool d, string g, bool e);
-            //:Libro(t,a,p),genero(g),es_saga(e){};
-            //Se puede definir desde aquí pero es mejor abajo
             
-            string getGenero();
-            bool getEs_saga();
+            string get_genero();
+            bool get_es_saga();
 
-            void setGenero(string gender);
-            void setEs_saga(bool saga);
+            void set_genero(string gender);
+            void set_es_saga(bool saga);
 
 };
 
+/**
+ * Constructor por default
+ *
+ * @param
+ * @return Objeto Novela
+ */
 Novela::Novela() : Libro(){
     genero = "Desconocido";
     es_saga = false;
 }
 
+/**
+ * Constructor que recibe valores para llenar las variables de instancia
+ * de este objeto, además de las variables heredadas
+ *
+ * @param string string t: titulo del libro, string a: autor, bool d: disponibilidad
+ * string g: genero del libro, bool e: indica si es parte de una saga o no
+ * @return objeto Novela
+ */
 Novela::Novela(string t, string a, bool d, string g, bool e): Libro(t,a,d){
     genero = g;
     es_saga = e;
 }
 
-string Novela::getGenero(){
+/**
+ * getter genero
+ *
+ * @param
+ * @return string: genero de la novela 
+*/
+string Novela::get_genero(){
     return genero;
 }
 
-bool Novela::getEs_saga(){
+/**
+ * getter es saga
+ *
+ * @param
+ * @return bool: true: es parte de una saga, false: no lo es
+*/
+bool Novela::get_es_saga(){
     return es_saga;
 }
 
-void Novela::setGenero(string gender){
+/**
+ * setter genero
+ *
+ * @param string gender: genero de la novela
+ * @return 
+*/
+void Novela::set_genero(string gender){
     genero = gender;
 }
 
-void Novela::setEs_saga(bool saga){
+/**
+ * setter es saga
+ *
+ * @param bool saga: true or false dependiendo si sea parte de una
+ * saga o no
+ * @return 
+*/
+void Novela::set_es_saga(bool saga){
     es_saga = saga;
 }
