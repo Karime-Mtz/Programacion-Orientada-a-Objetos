5bb7cb2637b1e443430ab4ce0f91b53c60d321aa
Karime-Mtz <A01709690@tec.mx>
2025-12-04 12:53:23 -0600

    Update Usuario.h

--- Usuario.h
+++ Usuario.h
@@ -1,81 +1,156 @@
+/*
+ * Proyecto Biblioteca Digital
+ * Karime Martínez López
+ * A01709690
+ * 03/12/2025
+ */
+
+ /*
+ * Clase Usuario lleva un registro de los libros que
+ * se piden prestados y se devuelven. También contiene
+ * los métodos genéricos para el manejo de los atributos
+ * del usuario.
+ *
+ */
+
 #include <string>
 #include <iostream>
 #include <list>
 #include "Libro.h"
 using namespace std;
 
+// Declaracion de clase Usuario
 class Usuario {
+
     private:
+        // Declaracion de variables de instancia
         string nombre;
         int edad;
         Libro libros_prestados[10];
         int cantidad_prestados;
 
     public:
+        // Declaracion de los métodos del objeto
+        Usuario();
         Usuario(string name, int age, int prestados);
 
-        string infoUsuario();
+        string info_usuario();
 
-        string getNombre();
-        int getEdad();
-        int getCantidad_prestados();
-        string mostrarLibros_prestados();
+        string get_nombre();
+        int get_edad();
+        int get_cantidad_prestados();
+        string mostrar_libros_prestados();
 
-        void setNombre(string name);
-        void setEdad(int age);
+        void set_nombre(string name);
+        void set_edad(int age);
         void agregar_libro_prestado(Libro &b);
         bool devolver_libro(string titulo);
-        void setCantidad_prestados(int n);
+        void set_cantidad_prestados(int n);
 };
 
+/**
+ * Constructor por default
+ *
+ * @param
+ * @return Objeto Usuario
+ */
+Usuario::Usuario(){
+    nombre = "Desconocido";
+    edad = 0;
+    cantidad_prestados = 0;
+}
 
+/**
+ * Constructor que recibe valores para llenar las variables de instancia
+ *
+ * @param string name: nombre del usuario, int age: edad de la persona, 
+ * int prestados: cantidad de libros que ha pedido prestados
+ * @return objeto Usuario
+ */
 Usuario::Usuario(string name, int age, int prestados){
     nombre = name;
     edad = age;
     cantidad_prestados = prestados;
 }
 
-string Usuario::infoUsuario(){
+/**
+ * Almacena los valores de las variables de instancia en una cadena de
+ * texto, así como el titulo de los libros que han sido prestados.
+ *
+ * @param
+ * @return string con los atributos del usuario y los libros que ha 
+ * pedido prestados
+*/
+string Usuario::info_usuario(){
     stringstream datos_usuario;
 
     datos_usuario << "\nNombre: " << nombre << "\n";
     datos_usuario << "Edad: " << edad << "\n";
     datos_usuario << "Libros prestados: " << "\n";
     for(int i = 0; i < cantidad_prestados; ++i) {
-        datos_usuario << libros_prestados[i].getTitulo() << "\n";
+        datos_usuario << libros_prestados[i].get_titulo() << "\n";
     }
     return datos_usuario.str();
 }
 
-string Usuario::getNombre(){
+/**
+ * getter nombre
+ *
+ * @param
+ * @return string: nombre del usuario
+*/
+string Usuario::get_nombre(){
     return nombre;
 }
 
-int Usuario::getEdad(){
+/**
+ * getter edad
+ *
+ * @param
+ * @return int: edad del usuario
+*/
+int Usuario::get_edad(){
     return edad;
 }
 
-int Usuario::getCantidad_prestados(){
+/**
+ * getter cantidad de libros prestados
+ *
+ * @param
+ * @return int cantidad de elementos de la lista
+*/
+int Usuario::get_cantidad_prestados(){
     return cantidad_prestados;
 }
 
-string Usuario::mostrarLibros_prestados(){
-    stringstream libros;
-    libros << "\nLibros prestados:" << "\n";
-    for(int i = 0; i < cantidad_prestados; ++i) {
-        libros << libros_prestados[i].getTitulo() << "\n";
-    }
-    return libros.str();
-}
-
-void Usuario::setNombre(string name){
+/**
+ * setter nombre
+ *
+ * @param string: nombre del usuario
+ * @return 
+*/
+void Usuario::set_nombre(string name){
     nombre = name;
 }
 
-void Usuario::setEdad(int age){
+/**
+ * setter edad
+ *
+ * @param int: edad del usuario
+ * @return 
+*/
+void Usuario::set_edad(int age){
     edad = age;
 }
 
+/**
+ * Lleva un registro de los libros que son pedidos prestados
+ * por el usuario. Agrega un objeto libro a la lista tipo libro
+ * y actualiza la disponiblidad del libro.
+ *
+ * @param Libro: objeto libro por referencia
+ * @return 
+*/
 void Usuario::agregar_libro_prestado(Libro &b){
     if(cantidad_prestados < 10){
         libros_prestados[cantidad_prestados] = b;
@@ -84,11 +159,31 @@ void Usuario::agregar_libro_prestado(Libro &b){
     }
 }
 
+/**
+ * Devuelve un libro previamente prestado. Busca el titulo del libro
+ * dentro de la lista de libros prestados, comparando en minúsculas y 
+ * lo elimina de la lista. Tambien actualiza la disponibilidad del libro.
+ *
+ * @param string: titulo del libro que se quiere devolver
+ * @return true: si el libro fue encontrado y devuelto, 
+ * false: no se encontró el libro
+*/
+
 bool Usuario::devolver_libro(string titulo){
+    // Convertir el titulo ingresado a minúsculas
+    for (char &c : titulo) {
+        c = tolower(c);
+    }
+
     for(int i = 0; i < cantidad_prestados; i++){
 
-        if(libros_prestados[i].getTitulo() == titulo){
-            libros_prestados[i].set_disponible(true);
+        // Obtener el título del libro y convertirlo a minúsculas
+        string titulo_libro = libros_prestados[i].get_titulo();
+        for (char &c : titulo_libro) {
+            c = tolower(c);
+        }
+
+        if (titulo_libro == titulo){
             for(int j = i; j < cantidad_prestados - 1; j++){
                 libros_prestados[j] = libros_prestados[j + 1];
             }
@@ -97,9 +192,16 @@ bool Usuario::devolver_libro(string titulo){
             return true;
         }
     }
+
     return false; // no se encontró el libro
 }
 
-void Usuario::setCantidad_prestados(int n){
+/**
+ * setter cantidad de libros prestados
+ *
+ * @param int: cantidad de libros prestados
+ * @return 
+*/
+void Usuario::set_cantidad_prestados(int n){
     cantidad_prestados = n;
 }
