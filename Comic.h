1b6dbc9fbec03ae9adee345cfa9611634a6ac7c4
Karime-Mtz <A01709690@tec.mx>
2025-12-04 12:54:15 -0600

    Update Comic.h

--- Comic.h
+++ Comic.h
@@ -1,32 +1,76 @@
+/*
+ * Proyecto Biblioteca Digital
+ * Karime Martínez López
+ * A01709690
+ * 03/12/2025
+ */
+
+ /*
+ * Clase Comic que hereda de clase Libro. Contiene los métodos
+ * genéricos para manejar sus atributos especiales.
+ *
+ */
+
 #include <string>
 #include <iostream>
 #include "Libro.h"
 using namespace std;
 
+// Declaracion de la clase Comic que hereda de clase Libro
 class Comic: public Libro{
+
     private:
+        // Declaracion de variables de instancia
         string universo; 
+
     public:
-            Comic();
-            Comic(string t, string a, bool d, string u);
+        // Declaracion de los metodos del objeto
+        Comic();
+        Comic(string t, string a, bool d, string u);
             
-            string getUniverso();
-            void setUniverso(string u);
+        string get_universo();
+        void set_universo(string u);
 };
 
+/**
+ * Constructor por default
+ *
+ * @param
+ * @return Objeto Comic
+ */
 Comic::Comic() : Libro(){
     universo = "Desconocido";
 }
 
+/**
+ * Constructor que recibe valores para llenar las variables de instancia
+ * de este objeto, además de las variables heredadas
+ *
+ * @param string string t: titulo del libro, string a: autor, bool d: disponibilidad
+ * string u: universo del comic
+ * @return objeto Comic
+ */
 Comic::Comic(string t, string a, bool d, string u) : Libro(t,a,d){
     universo = u;
 }
 
-string Comic::getUniverso(){
+/**
+ * getter universo
+ *
+ * @param
+ * @return string: universo del comic
+*/
+string Comic::get_universo(){
     return universo;
 }
 
-void Comic::setUniverso(string u){
+/**
+ * setter universo
+ *
+ * @param string u: universo al que pertenece el comic
+ * @return 
+*/
+void Comic::set_universo(string u){
     universo = u;
 }
 
